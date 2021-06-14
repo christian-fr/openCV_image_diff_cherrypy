036e638e2b32cff9119f1db715454c2cd70e26fc
christian-fr <karl.wilhelm@posteo.de>
2021-06-14 11:54:53 +0200

    cleaned up openCV_diff_classes.py

--- openCV_diff_classes.py
+++ openCV_diff_classes.py
@@ -1,29 +1,13 @@
-# source: https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
-
-# import the necessary packages
 from skimage.metrics import structural_similarity as ssim
-# import argparse
 import imutils
-# import os
+from pathlib import Path
+from typing import Union
+import logging
+
 # to install opencv2:
 # $ pip install opencv-python
 import cv2
 
-from pathlib import Path
-from typing import Union
-# from io import BytesIO
-import logging
-
-import base64
-
-
-# # construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-f", "--first", required=True,
-# 	help="first input image")
-# ap.add_argument("-s", "--second", required=True,
-# 	help="second")
-# args = vars(ap.parse_args())
 
 class OpenCVDiff:
     def __init__(self, file1: Union[Path, str],
@@ -41,60 +25,52 @@ class OpenCVDiff:
         self.output_path1 = output_path1
         self.output_path2 = output_path2
 
-        # load the two input images
-        self.imageA = cv2.imread(file1)
-        self.imageB = cv2.imread(file2)
+        # read the images
+        self.input_image_1 = cv2.imread(file1)
+        self.input_image_2 = cv2.imread(file2)
+
         # convert the images to grayscale
-        self.grayA = cv2.cvtColor(self.imageA, cv2.COLOR_BGR2GRAY)
-        self.grayB = cv2.cvtColor(self.imageB, cv2.COLOR_BGR2GRAY)
+        self.input_image_1_grayscale = cv2.cvtColor(self.input_image_1, cv2.COLOR_BGR2GRAY)
+        self.input_image_2_grayscale = cv2.cvtColor(self.input_image_2, cv2.COLOR_BGR2GRAY)
 
-        # compute the Structural Similarity Index (SSIM) between the two
-        # images, ensuring that the difference image is returned
-        (score, diff) = ssim(self.grayA, self.grayB, full=True)
-        diff = (diff * 255).astype("uint8")
-        print("SSIM: {}".format(score))
+        # calculate structural similarity index
+        score, difference = ssim(self.input_image_1_grayscale, self.input_image_2_grayscale, full=True)
+        difference = (255 * difference).astype("uint8")
+        self.logger.info(f'SSIM: {score}')
 
-        # threshold the difference image, followed by finding contours to
-        # obtain the regions of the two input images that differ
-        thresh = cv2.threshold(diff, 0, 255,
+        # threshold the difference image
+        thresh = cv2.threshold(difference, 0, 255,
                                cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+        # searching for contours of differing regions
         contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                     cv2.CHAIN_APPROX_SIMPLE)
+
         contours = imutils.grab_contours(contours)
 
         # loop over the contours
         for c in contours:
-            # compute the bounding box of the contour and then draw the
-            # bounding box on both input images to represent where the two
-            # images differ
+            # create a bounding rectangle around the contour
             (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(self.imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.rectangle(self.imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.rectangle(self.input_image_1, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.rectangle(self.input_image_2, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-        # save png images as base64 encoded strings
-        retval_a, buffer_a = cv2.imencode('.png', self.imageA)
-        retval_b, buffer_b = cv2.imencode('.png', self.imageB)
-        self.buffer_a_as_text = base64.b64encode(buffer_a)
-        self.buffer_b_as_text = base64.b64encode(buffer_b)
+        # # DEV save png images as base64 encoded strings
+        # retval_a, buffer_a = cv2.imencode('.png', self.input_image_1)
+        # retval_b, buffer_b = cv2.imencode('.png', self.input_image_2)
+        # self.buffer_a_as_text = base64.b64encode(buffer_a)
+        # self.buffer_b_as_text = base64.b64encode(buffer_b)
 
     def write_images_to_output_paths(self):
-
         # write the output images
         if self.output_path1 is not None:
-            cv2.imwrite(self.output_path1, self.imageA)
+            cv2.imwrite(self.output_path1, self.input_image_1)
         if self.output_path2 is not None:
-            cv2.imwrite(self.output_path2, self.imageB)
+            cv2.imwrite(self.output_path2, self.input_image_2)
 
-        # cv2.imshow("Original", imageA)
-        # cv2.imshow("Modified", imageB)
-        # cv2.imshow("Diff", diff)
-        # cv2.imshow("Thresh", thresh)
-        # cv2.waitKey(0)
-
-
-
-    def return_base64_string_tuple(self):
-        return self.buffer_a_as_text, self.buffer_b_as_text
+    # # DEV tried out to return the images as base64 strings
+    # def return_base64_string_tuple(self):
+    #     return self.buffer_a_as_text, self.buffer_b_as_text
 
     def startup_logger(self, log_level=logging.DEBUG):
         """
@@ -107,7 +83,3 @@ class OpenCVDiff:
                                       '%(levelname)-8s\t%(message)s')
         fh.setFormatter(fh_format)
         self.logger.addHandler(fh)
-
-# usage:
-# $ python image_diff.py --first images/original_02.png
-# 	--second images/modified_02.png
